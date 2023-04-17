4b512f2d679c1d5ee60d7c96ccf459b0141382c4
Germo Mäenurm <90237415+germomaenurm@users.noreply.github.com>
2023-04-17 14:10:38 +0300

    tulemuste telefoni vaate smol fix (heavy)

--- anett-koopia/src/App.js
+++ anett-koopia/src/App.js
@@ -558,7 +558,7 @@ function App() {
               className="w-1/2 bg-[url('https://www.anettkontaveit.ee/images/tulemused-bg.jpg')] lg:bg-no-repeat lg:bg-right bg-[auto_100%] lg:h-[800px] bg bg-top-left h-[300px] bg-no-repeat"
             ></div>
             <div id='results-content' className='w-1/2 lg:pt-[100px] self-start pt-[10px]'>
-              <div id='results' className='w-full max-w-[580px] pr-7'>
+              <div id='results' className='w-full lg:max-w-[580px] lg:pr-7 pr-[20px] pl-[20px] mt-0 mr-auto mb-0 ml-auto'>
                 <h2 className='text-5xl text-blue-700 font-zonaBold mb-8 text-left'>Tulemused</h2>
                 <div id='results-wrapper'>
                   <div
