232ac9a9e124e50e04a2d705ced1d897a8f37308
Germo Mäenurm <90237415+germomaenurm@users.noreply.github.com>
2023-04-17 14:14:11 +0300

    tulemuste telefoni vaate fix (heavy)

--- anett-koopia/src/App.js
+++ anett-koopia/src/App.js
@@ -557,7 +557,7 @@ function App() {
               id='results-image'
               className="w-1/2 bg-[url('https://www.anettkontaveit.ee/images/tulemused-bg.jpg')] lg:bg-no-repeat lg:bg-right bg-[auto_100%] lg:h-[800px] bg bg-top-left h-[300px] bg-no-repeat"
             ></div>
-            <div id='results-content' className='w-1/2 lg:pt-[100px] self-start pt-[10px]'>
+            <div id='results-content' className='lg:w-1/2 lg:pt-[100px] lg:self-start pt-[10px]'>
               <div id='results' className='w-full lg:max-w-[580px] lg:pr-7 pr-[20px] pl-[20px] mt-0 mr-auto mb-0 ml-auto'>
                 <h2 className='text-5xl text-blue-700 font-zonaBold mb-8 text-left'>Tulemused</h2>
                 <div id='results-wrapper'>
